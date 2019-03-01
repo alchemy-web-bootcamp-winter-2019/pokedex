76cf705458750e211b5ba7517b5eef9f4981fa15
Cara Ponzini <ponzini@gmail.com>
2019-03-01 11:20:25 -0800

    test for template created and passed

--- test/tests.js
+++ test/tests.js
@@ -14,3 +14,45 @@ const pokemon = {
     'color_f': '#81A763',
     'url_image': 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'
 };
+
+function makePokeTemplate(pokemon) {
+    let htmlTemplate = `
+    <li style=" border: 5px solid ${pokemon.color_1};">
+        <h2>${pokemon.pokemon} <span class="type_2" style=" background-color: ${pokemon.color_2};"></span></h2>
+        <img src="${pokemon.url_image}" alt="${pokemon.pokemon}">
+        <div>
+        HP: <span id="hp">${pokemon.hp}</span>
+        Attack: <span id="attack">${pokemon.attack}</span>
+        Defense: <span id="defense">${pokemon.defense}</span>
+        </div>
+    </li>`;
+
+    const template = document.createElement('template');
+    template.innerHTML = htmlTemplate;
+
+    return template.content;
+
+}
+
+test('function produces pokemon template', assert => {
+    //arrange
+    const html = `
+    <li style=" border: 5px solid #78C850;">
+        <h2>bulbasaur <span class="type_2" style=" background-color: #A040A0;"></span></h2>
+        <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" alt="bulbasaur">
+        <div>
+        HP: <span id="hp">45</span>
+        Attack: <span id="attack">49</span>
+        Defense: <span id="defense">49</span>
+        </div>
+    </li>`;
+
+    //act
+    const result = makePokeTemplate(pokemon);
+
+    //assert
+    assert.htmlEqual(result, html);
+
+
+});
+
