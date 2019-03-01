1b87b33d5f27c501bc6885237a7034e66fc36d38
Cheri Portman <cheriportman@hotmail.com>
2019-03-01 08:50:56 -0800

    1st test redux, initially set up wrong. Hard-coded test passes.

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,9 @@
 import './html-equal.js';
+import './card-component.js';
 
-const test = QUnit.test;
-QUnit.module('pokeman card creation');
+// const test = QUnit.test;
+// QUnit.module('pokeman card creation');
 
-QUnit.test('delete me and write a test file', assert => {
-    assert.equal(true, true);
-});
+// QUnit.test('delete me and write a test file', assert => {
+//     assert.equal(true, true);
+// });
