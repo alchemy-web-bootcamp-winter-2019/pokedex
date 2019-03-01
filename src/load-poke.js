410916f8db379bb6691620841ae85d08b301eea6
Cara Ponzini <ponzini@gmail.com>
2019-03-01 11:42:38 -0800

    created paging buttons and display

--- src/load-poke.js
+++ src/load-poke.js
@@ -3,7 +3,11 @@ import makePokeTemplate from './make-poke-template.js';
 const pokemonGallery = document.getElementById('pokemon-gallery');
 
 export default function loadPoke(pokemon) {
-    
+
+    // while(pokemonGallery.children.length > 0){
+    //     pokemonGallery.lastElementChild.remove();
+    // }
+
     pokemon.forEach(pokemon => {
         const dom = makePokeTemplate(pokemon);
         pokemonGallery.appendChild(dom);
