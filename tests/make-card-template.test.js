26b80de99ce7c216ed9d01654e4df514ca2d0af9
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-03-01 11:52:27 -0800

    Added dynamic type 2 template generation, TRICKY

--- tests/make-card-template.test.js
+++ tests/make-card-template.test.js
@@ -6,21 +6,26 @@ function makeCardTemplate(pokemon) {
       <li>
          <h2>${pokemon.pokemon}</h2>
             <img src="${pokemon.url_image}">
-            <p> <span class="type1">${pokemon.type_1}</span> <span class="type2">${pokemon.type_2}</span> </p>
+            <p>
+            <span class="type1">${pokemon.type_1}</span>
+            ${ pokemon.type_2 === 'NA' ? '' : `<span class="type2">${pokemon.type_2}</span>` }
+            </p>
         </li>
     `;
    const template = document.createElement('template');
-   template.innerHTML = html;
+   template.innerHTML = html; 
    return template.content;
 }
 
-test('Template test', function(assert) {
+test('Template test for 2 types', function(assert) {
     // Arrange
    const expected = /*html*/`
 		<li>
 			<h2>bulbasaur</h2>
 			<img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png">
-			<p> <span class="type1">grass</span> <span class="type2">poison</span> </p>
+         <p>
+            <span class="type1">grass</span> <span class="type2">poison</span>
+         </p>
 		</li>
 	`;
    const pokemon = {
@@ -28,6 +33,29 @@ test('Template test', function(assert) {
       type_1: 'grass',
       type_2: 'poison',
       url_image: 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'
+   };
+	// Act
+   const result = makeCardTemplate(pokemon);
+	// Assert
+   assert.htmlEqual(result, expected);
+});
+
+test('Template test for 1 type', function(assert) {
+    // Arrange
+   const expected = /*html*/`
+		<li>
+			<h2>pikachu</h2>
+			<img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png">
+         <p>
+            <span class="type1">electric</span>
+         </p>
+		</li>
+	`;
+   const pokemon = {
+      pokemon: 'pikachu',
+      type_1: 'electric',
+      type_2: 'NA',
+      url_image: 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png'
    };
 	// Act
    const result = makeCardTemplate(pokemon);
