62666e17a75104e7e7db9b395ba79d1267b02043
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-03-01 14:34:57 -0800

    buttons can now update page-nav

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import loadDisplay from './display-component.js';
 import pokemonList from '../data/pokedex.js';
+import { changePage } from './paging-component.js';
 
 loadDisplay(pokemonList);
+changePage(pokemonList);
--- src/paging-component.js
+++ src/paging-component.js
@@ -1,5 +1,32 @@
 const prevButton = document.getElementById('previous');
 const nextButton = document.getElementById('next');
 const currentPage = document.getElementById('current-page');
+const totalPages = document.getElementById('total-pages');
+
+let currentPageNumber = 1;
+currentPage.textContent = currentPageNumber;
+
+export function changePage(pokeList) {
+   const PER_PAGE = 100;
+   let totalPagesNumber = Math.ceil(pokeList.length / PER_PAGE);
+   totalPages.textContent = totalPagesNumber;
+   updatePageNav();
+   nextButton.addEventListener('click', () => {
+      currentPageNumber++;
+      updatePageNav();
+   });
+   prevButton.addEventListener('click', () => {
+      currentPageNumber--;
+      updatePageNav()
+   });
+
+   function updatePageNav () {
+      prevButton.disabled = currentPageNumber === 1;
+      nextButton.disabled = currentPageNumber === totalPagesNumber;
+      currentPage.textContent = currentPageNumber;
+   }
+}
+   
+
 
 
