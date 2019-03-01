96dde13791784d8316e2a7487e7f051d0d277d16
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-03-01 14:14:02 -0800

    passing a second page-array test

--- test/page-array.test.js
+++ test/page-array.test.js
@@ -31,3 +31,17 @@ test('page 1 of 3 per page', assert => {
     //assert
     assert.deepEqual(paged, ['one', 'two', 'three']);
 });
+
+test('page 2 of 5 per page', assert => {
+    //arrange
+    const pagingOptions = {
+        page: 2,
+        perPage: 5
+    };
+
+    //act
+    const paged = pageArray(testArray, pagingOptions);
+
+    //assert
+    assert.deepEqual(paged, ['six', 'seven']);
+});
