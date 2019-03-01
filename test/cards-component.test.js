bb47634dfd72d91a01ee58512eb1c4aa480fac41
Leslie Shea <leslieyshea@gmail.com>
2019-03-01 13:38:36 -0800

    updated to add a tag

--- test/cards-component.test.js
+++ test/cards-component.test.js
@@ -10,20 +10,23 @@ const pokedex = {
     'url_image': 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
     'hp': 45,
     'attack': 49,
-    'defense': 49
+    'defense': 49,
+    'pokedex': 'http://www.pokemon.com/us/pokedex/bulbasaur'
 };
 
 test('make pokedex template', assert => {
     const result = makePokedexTemplate(pokedex);
     const expected = /*html*/
     `<li style="background-color: #78C850">
-        <h3>Bulbasaur<div class="type_2" style="background-color: #A040A0"></div></h3>
-        <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png">
-        <p class="stats">
-            <span>HP: 45 | </span>
-            <span>A: 49 | </span>
-            <span>D: 49</span>
-        </p>
+        <a href="http://www.pokemon.com/us/pokedex/bulbasaur" target="_blank">
+            <h3>Bulbasaur<div class="type_2" style="background-color: #A040A0"></div></h3>
+            <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png">
+            <p class="stats">
+                <span>HP: 45 | </span>
+                <span>A: 49 | </span>
+                <span>D: 49</span>
+            </p>
+        </a>
     </li>`;
 
     assert.htmlEqual(result, expected);
