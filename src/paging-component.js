e06ed645939f7372d3cdf70be1cf09cdae8fe5ad
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-03-01 13:59:11 -0800

    refactored loadPaging function

--- src/paging-component.js
+++ src/paging-component.js
@@ -5,33 +5,33 @@ const totalPagesSpan = document.getElementById('total-pages');
 
 const PER_PAGE = 22;
 
+
 export default function loadPaging(totalCount) {
+    
     const totalPageCount = Math.ceil(totalCount / PER_PAGE);
     totalPagesSpan.textContent = totalPageCount;
     let currentPageNumber = 1;
-    currentPageSpan.textContent = currentPageNumber;
-
+    updatePaging();
+    
+    function updatePaging() {
+        currentPageSpan.textContent = currentPageNumber;
+        const pagingOptions = {
+            page: currentPageNumber,
+            perPage: PER_PAGE
+        };
+        console.log(pagingOptions);
+        nextButton.disabled = currentPageNumber === totalPageCount;
+        previousButton.disabled = currentPageNumber === 1;
+    }
+    
     nextButton.addEventListener('click', () => {
         currentPageNumber++;
-        currentPageSpan.textContent = currentPageNumber;
-        const pagingOptions = {
-            page: currentPageNumber,
-            perPage: PER_PAGE
-        };
-        console.log(pagingOptions);
-        nextButton.disabled = currentPageNumber === totalPageCount;
-        previousButton.disabled = currentPageNumber === 1;
+        updatePaging();
     });
-
+    
     previousButton.addEventListener('click', () => {
         currentPageNumber--;
-        currentPageSpan.textContent = currentPageNumber;
-        const pagingOptions = {
-            page: currentPageNumber,
-            perPage: PER_PAGE
-        };
-        console.log(pagingOptions);
-        nextButton.disabled = currentPageNumber === totalPageCount;
-        previousButton.disabled = currentPageNumber === 1;
+        updatePaging();
     });
 }
+
