f7c51b4e6bd06c42d5b1501541713fd409d6b832
Chris Piccaro <chrispiccaro18@gmail.com>
2019-02-28 20:04:10 -0800

    Working on paging-component.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import pokedex from '../data/pokedex.js';
 import loadCards from './card-component.js';
+import loadPaging from './paging-component.js';
 
+loadPaging(pokedex.length);
 loadCards(pokedex);
