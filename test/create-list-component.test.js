a301e27b217a82917a3510795b610bbfbc819b6c
ollinieminen <otn987@gmail.com>
2019-02-28 17:27:21 -0800

    create a failing test for create-list-component

---
+++ test/create-list-component.test.js
@@ -0,0 +1,11 @@
+const test = QUnit.test;
+
+test('test templat making machine', assert => {
+    //assign
+    const expected = `
+         
+        `;
+    //act
+    //assert
+    assert.deepEqual(actual, expected);
+});
