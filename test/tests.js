a64c5b37d0ab9034adb838c01eb05496d3962231
Cara Ponzini <ponzini@gmail.com>
2019-03-01 11:07:52 -0800

    hard coded pokemon display in HTML

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,16 @@
 import './html-equal.js';
 
-QUnit.test('delete me and write a test file', assert => {
-    assert.equal(true, true);
-});
+const test = QUnit.test;
+
+const pokemon = {
+    'pokemon': 'bulbasaur',
+    'type_1': 'grass',
+    'type_2': 'poison',
+    'attack': 49,
+    'defense': 49,
+    'hp': 45,
+    'color_1': '#78C850',
+    'color_2': '#A040A0',
+    'color_f': '#81A763',
+    'url_image': 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'
+};
