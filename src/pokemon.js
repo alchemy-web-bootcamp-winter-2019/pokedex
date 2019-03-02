59cf9c9c1061d5443a502b1a8a882fe54b68af6d
Leslie Shea <leslieyshea@gmail.com>
2019-03-01 17:33:15 -0800

    added username to header

--- src/pokemon.js
+++ src/pokemon.js
@@ -2,6 +2,15 @@ import pokedex from '../data/pokedex.js';
 import loadPokedex from '../src/cards-component.js';
 import loadPaging from '../src/paging-component.js';
 import pageArray from '../src/page-array.js';
+import userNameDisplay from '../src/userNameDisplay.js';
+
+const json = window.localStorage.getItem('user');
+if(!json) {
+    window.location = '/';
+}
+const user = JSON.parse(json);
+
+userNameDisplay(user);
 
 loadPaging(pokedex.length, pagingOptions => {
     const pagedPokedex = pageArray(pokedex, pagingOptions);
