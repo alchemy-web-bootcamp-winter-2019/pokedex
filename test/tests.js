44bf9b80c8313da157753b4ea2e85a1a15ddf738
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-03-03 14:17:40 -0800

    completed sorting function testing and migrated to its own js file

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,7 @@
 import './html-equal.js';
-import '../test/template-component.test.js';
-import '../test/pokedex-component.test.js';
-import '../test/sort-component.test';
+// import '../test/template-component.test.js';
+// import '../test/pokedex-component.test.js';
+import '../test/sort-component.test.js';
 
 QUnit.test('delete me and write a test file', assert => {
     assert.equal(true, true);
