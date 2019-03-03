3d6c8757386ce5e58a59b997db7dd6ab8027ee3f
Cheri Portman <cheriportman@hotmail.com>
2019-03-03 11:37:46 -0800

    filterPokedex test passes with one or no parameter

--- test/filter-pokedex.test.js
+++ test/filter-pokedex.test.js
@@ -2,23 +2,51 @@ const test = QUnit.test;
 QUnit.module('construct filter based on selected type');
 
 
-function filterPokedex(testArray) {
-    return [{ type_1: 'poison' }];
+function filterPokedex(testArray, chosenType) {
+    return testArray.filter(item => {
+        const hasType = !chosenType.type_1 || chosenType.type_1 === item.type_1;
+        console.log('hasType', hasType);
+        return hasType;
+    });
 }
 
-test('filtered based on type', assert => {
+
+test('filtered based on poison', assert => {
     //arrange    
     const testArray = [
-        { type_1: 'poison' },
-        { type_1: 'grass' },
-        { type_1: 'fire' } 
+        { type_1: 'poison', id: 1 },
+        { type_1: 'grass', id: 4 },
+        { type_1: 'fire', id: 3 } 
     ];
 
+    const chosenType = { type_1: 'poison' };
+
     const expected = [
-        { type_1: 'poison' }
+        { type_1: 'poison', id: 1 }
     ];
     //act
-    const result = filterPokedex(testArray);
+    const result = filterPokedex(testArray, chosenType);
+
+    //assert
+    assert.deepEqual(result, expected);
+});
+test('filtered based on no choice made', assert => {
+    //arrange    
+    const testArray = [
+        { type_1: 'poison', id: 1 },
+        { type_1: 'grass', id: 4 },
+        { type_1: 'fire', id: 3 } 
+    ];
+
+    const chosenType = { type_1: '' };
+
+    const expected = [
+        { type_1: 'poison', id: 1 },
+        { type_1: 'grass', id: 4 },
+        { type_1: 'fire', id: 3 } 
+    ];
+    //act
+    const result = filterPokedex(testArray, chosenType);
 
     //assert
     assert.deepEqual(result, expected);
