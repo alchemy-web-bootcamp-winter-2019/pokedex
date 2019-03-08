9eee7363f7ec54981775c04282348986b53311dc
ollinieminen <otn987@gmail.com>
2019-03-07 16:54:18 -0800

    finished

--- test/filter.test.js
+++ test/filter.test.js
@@ -1,9 +1,32 @@
 const test = QUnit.test;
 
-test('test filter', assert=>{
-//assign
-//act
-//test
+test('test filter', assert => {
+    //assign
+    const filterTerm = 
+    const pokeArray =
+        [
+            {
+                'pokemon': 'bulbasaur',
+                'attack': 49,
+                'defense': 49,
+                'hp': 45,
+            },
+            {
+                'pokemon': 'ivysaur',
+                'attack': 62,
+                'defense': 63,
+                'hp': 60,
+            },
+            {
+                'pokemon': 'hitmonlee',
+                'attack': 82,
+                'defense': 83,
+                'hp': 80,
+            }];
 
-assert(actual, expected);
+    //act
+
+    //test
+
+    assert(actual, expected);
 });
